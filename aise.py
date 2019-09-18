c29821d58d3b8ac78ce036ce428260810c268665
sidd315 <sid.thakkar97@gmail.com>
2019-09-18 13:54:14 -0500

    kuch toh bhi

--- aise.py
+++ aise.py
@@ -3,9 +3,9 @@ if bhukkad=="YES":
     print("Kuch khaale bc")
     print("Par veg hona chaiye")
 else:
-    print("jaa hagke aa")
-    print("tissue lekar jaana")
-    print("this is the new line that i originally typed in thirsty")
+    print("jaa aur baadmei aa")
+    print("abhi he jaa")
+    print("hahahaha")
 
 
 
